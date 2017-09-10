a127bdac9b98e97abb94286db1a9f26051a41f06
Den Neb <deneb42@gmail.com>
2017-09-10 23:25:24 +0300

    wo

--- b2/main.js
+++ b2/main.js
@@ -1,50 +1,98 @@
 (() => {
+class BaseNode {
+	constructor(b2) {
+		this.b2 = b2;
+	}
+	getInput() {
+		return null;
+	}
+	getOutput() {
+		return null;
+	}
+}
+class Attr extends BaseNode {
+	constructor(b2, node, name) {
+		super(b2);
+		this.set = (val) => node[name] = b2.normVal(val).v;
+	}
+}
 class B2 {
 	constructor (context) {
 		this.context = context || new AudioContext();
 		this.nodeTypes = {};
+		this.onCreate = {
+			Oscillator: (node, b2) => node.start(),
+		};
 	}
-	getNodeKind(node) {
+	getNodeKinds(node) {
 		if (typeof node === 'object') {
 			if (window.Kefir) {
-				if (node instanceof Kefir.Observable) return 'kefir';
+				if (node instanceof Kefir.Observable) return ['kefir', 'isOutput'];
 			}
-			if (node instanceof AudioParam) return 'aparam';
-			if (node instanceof AudioNode) return 'anode';
+			if (node instanceof AudioParam) return ['aparam', 'isInput'];
+			if (node instanceof AudioNode) {
+				const res = ['anode', 'isInput', 'isOutput'];
+				if (node instanceof AudioScheduledSourceNode) res.push('triggerable');
+				return res;
+			}
+			if (node instanceof BaseNode) {
+				const res = [];
+				if (node.getInput()) res.push('hasInput');
+				if (node.getOutput()) res.push('hasOutput');
+				for (let proto = node; proto !== BaseNode; proto = Object.getPrototypeOf(proto)) {
+					res.push(proto.constructor.name.toLowerCase());
+				}
+				return res;
+			}
 			return 'unknown';
 		}
 		return typeof node;
 	}
+	getConnector(from, to) {
+		const fromKinds = this.getNodeKinds(from);
+		const toKinds = this.getNodeKinds(to);
+		for (let fk of fromKinds) {
+			for (let tk of toKinds) {
+				const func = this[`connect_${fk}2${tk}`];
+				if (func) return func;
+			}
+		}
+		throw `cannot connect from ${fromKinds[0]} to ${toKinds[0]}`;
+	}
 	connect(from, to) {
-		const fromKind = this.getNodeKind(from);
-		const toKind = this.getNodeKind(to);
-		const func = this[`connect_${fromKind}2${toKind}`];
-		if (!func) throw `cannot connect from ${fromKind} to ${toKind}`;
+		const func = this.getConnector(from, to);
+		func.call(this, from, to);
+	}
+	normVal(val) {
+		const ct = this.context.currentTime;
+		if (val && typeof val === 'object') {
+			const {t = ct, v = 0, k = ''} = val;
+			return {t, v, k};
+		}
+		if (typeof val === 'number') return {t: ct, v: val};
+		return {t: ct, v: 0};
 	}
 	setValueToParam(val, param) {
-		if (typeof val === 'number') {
-			param.value = val; return;
-		}
-		if (typeof val === 'object') {
-			let {t, v, k} = val;
-			if (!t) t = this.context.currentTime;
-			switch (k) {
-				case 'l':
-					param.linearRampToValueAtTime(v, t);
-				case 'e':
-				case 'x':
-					param.exponentialRampToValueAtTime(v, t);
-				case 's':
-				default:
-					param.setValueAtTime(v, t);
-					return;
-			}
+		let {t, v, k} = this.normVal(val);
+		if (!t) t = this.context.currentTime;
+		switch (k) {
+			case 'l':
+				param.linearRampToValueAtTime(v, t);
+			case 'e':
+			case 'x':
+				param.exponentialRampToValueAtTime(v, t);
+			case 's':
+			default:
+				param.setValueAtTime(v, t);
+				return;
 		}
 	}
 	makeNode(nodeType, options = {}) {
 		const nodeCons = this.nodeTypes[nodeType] || window[`${nodeType}Node`];
 		if (!nodeCons) throw `known node type: ${nodeType}`;
-		return new nodeCons(this.context, options);
+		const node = new nodeCons(this.context, options);
+		if (this.onCreate[nodeType]) this.onCreate[nodeType](node, this);
+		return node;
 	}
 	makeConst(v) {
 		return this.makeNode('ConstantSource', {offset: v});
@@ -56,11 +104,8 @@ class B2 {
 		from.onValue(v => this.setValueToParam(v, to));
 	}
 	connect_kefir2anode(from, to) {
-		const g = this.makeGain(0.0);
-		const c = this.makeConst(1.0);
-		c.connect(g);
-		g.connect(to);
-		connect_kefir2aparam(from, g.gain);
+		connect_kefir2aparam(from, c.offset);
+		c.connect(to);
 	}
 	connect_audio(from, to) {
 		from.connect(to);
@@ -76,6 +121,28 @@ class B2 {
 	}
 	connect_number2anode(from, to) {
 		this.makeConst(from).connect(to);
+	}
+	connect_kefir2triggerable(from, to) {
+		from.onValue(val => {
+			const {v, t} = this.normVal(val);
+			if (v) to.start(t);
+			else to.stop(t);
+		});
+	}
+	connect_kefir2attr(from, to) {
+		from.onValue(val => {
+			const {v} = this.normVal(val);
+			to.set(v);
+		});
+	}
+	connect_hasOutput2isInput(from, to) {
+		this.connect(from.getOutput(), to);
+	}
+	connect_isOutput2hasInput(from, to) {
+		this.connect(from, to.getInput());
+	}
+	connect_hasOutput2hasInput(from, to) {
+		this.connect(from.getOutput(), to.getInput());
 	}
 }
 
