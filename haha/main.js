b3db21caae2e7a56e40544910b3658a41d0e74a5
Deneb42 <deneb42@gmail.com>
2018-02-08 19:48:22 +0300

    select/delete

--- haha/main.js
+++ haha/main.js
@@ -1,6 +1,6 @@
 import { h, app } from './hyperapp/index.js';
 
-for (const tag of 'div,h1,button,svg,path,circle,g'.split(',')) {
+for (const tag of 'div,h1,button,svg,path,circle,g,text,textPath,defs,marker'.split(',')) {
   window['h' + tag] = (props, ...children) => h(tag, props, children);
 }
 
@@ -23,7 +23,7 @@ for (let i = 0; i < Math.random()*nodeCount*nodeCount; i++) {
   const fi = pick([2, 3]);
   const t = Math.floor(Math.random()*nodeCount);
   const ti = pick([0, 1]);
-  elems[`l${f}.${fi}_${t}.${ti}`] = {
+  elems[`l${f}.${fi}-${t}.${ti}`] = {
     _t: 'DirectLink', from: `c${f}`, to: `c${t}`, fromPort: fi, toPort: ti,
   };
 }
@@ -41,21 +41,26 @@ const actions = {
       return {...state, [_id]: {...state[_id], ...it}};
     },
     newOne: ({_id, ...it}) => state => ({...state, [_id]: {...state[_id], ...it}}),
-    setPortOver: (port) => state => {
+    setPortOver: (port) => (state, actions) => {
       console.log('setPortOver', port);
       if (port) {
         const {parent: {_id}, name} = port;
-        return {...state, __topState: {...state.__topState, portOverParent: _id, portOverName: name}};
+        //return {...state, __topState: {...state.__topState, portOverParent: _id, portOverName: name}};
+        return actions.setTopState({portOverParent: _id, portOverName: name});
       } else {
         return {...state, __topState: {...state.__topState, portOverParent: null, portOverName: null}};
       }
+    },
+    setTopState: (it) => state => {
+      console.log('topState', it);
+      return {...state, __topState: {...state.__topState, ...it}}
     },
     newLink: ({from, fromPort}) => (state, actions) => {
       const topState = state.__topState;
       const {portOverParent, portOverName} = topState;
       if (portOverParent) {
         const [to, toPort] = [portOverParent, portOverName];
-        return actions.setIt({_id: `l${from}.${fromPort}_${to}.${toPort}`,
+        return actions.setIt({_id: `l${from}.${fromPort}-${to}.${toPort}`,
           _t: 'DirectLink', from, fromPort, to, toPort
         });
       }
@@ -86,6 +91,9 @@ class Elem {
   }
   getLayer() {
     return this.layer || 0;
+  }
+  isSelected() {
+    return this.all.__topState.state.currentElem === this._id;
   }
 }
 
@@ -157,7 +165,7 @@ class Node extends Elem {
     //console.log({name, po: this.portOver, a: name === this.portOver});
     this.ports.push(new Port(this, {...state, name, isOver: name === this.portOver}));
   }
-  dragMouseDown({setIt}) {
+  dragMouseDown({setIt, setTopState, deleteOne}) {
     const {_id, x, y} = this;
     return startDragOnMouseDown(
       e => {
@@ -168,10 +176,33 @@ class Node extends Elem {
       (e, {dx, dy}) => {
         setIt({_id, x: snap(dx + e.x), y: snap(dy + e.y)});
       },
-      e => {
-        setIt({_id, dragging: false})
+      (e, {dx, dy}) => {
+        setIt({_id, dragging: false});
+        const [dx1, dy1] = [x - e.x, y - e.y];
+        if (dx1 === dx && dy1 === dy) {
+          console.log(this._id, 'click');
+          if (!e.shiftKey) setTopState({currentElem: _id});
+          if (e.shiftKey) {
+            deleteOne({_id: this._id});
+            Object.values(this.all)
+              .filter(elem => elem instanceof Link)
+              .map(deleteOne)
+          }
+        }
       }
     );
+  }
+  mainClick({setTopState}) {
+    return e => {
+      console.log(this._id, 'click');
+      if (!e.shiftKey) setTopState({currentElem: _id});
+      if (e.shiftKey) {
+        deleteOne({_id: this._id});
+        Object.values(this.all)
+          .filter(elem => elem instanceof Link)
+          .map(deleteOne)
+      }
+    }
   }
   isDragging() {
     return this.dragging;
@@ -188,7 +219,7 @@ class FakeNode extends Node {
   constructor (_id, all) {
     super(_id, all);
     this.portOver = 0;
-    this.addPort({dx: 0, dy: 0, fill: 'none'});
+    this.addPort({dx: 0, dy: -2, fill: 'none'});
   }
   isDragging() {
     return true;
@@ -214,9 +245,11 @@ class Circle extends Node {
   renderSVG(actions) {
     const {_id, x, y, r, fill, dragging} = this;
     return hg({}, 
-      hcircle({id: _id, cx: x, cy: y, r, fill, stroke: dragging ? 'black' : 'grey', 'stroke-width': dragging ? 2 : 1,
-        onmousedown: this.dragMouseDown(actions)
+      hcircle({id: _id, cx: x, cy: y, r, fill, stroke: dragging ? 'black' : '#444', 'stroke-width': this.isSelected() ? 5 : dragging ? 2 : 1,
+        onmousedown: this.dragMouseDown(actions),
+        onclick: e => console.log(e), //this.mainClick(actions),
       }),
+      htext({'text-anchor': 'middle', x, y, dy: 0, textLength: 2*r, 'dominant-baseline': 'middle'}, '. ' + _id + ' .'),
       this.ports.map(port => port.renderSVG(actions)),
     );
   }
@@ -241,17 +274,32 @@ class Link extends Elem {
 }
 
 class DirectLink extends Link {
-  renderSVG(actions) {
+  renderSVG({setIt, deleteOne}) {
+    const {_id, state: {over}} = this;
     const [from, to] = [
       this.all[this.from].getPort(this.fromPort),
       this.all[this.to].getPort(this.toPort),
     ];
     const dragging = this.isDragging();
-    return hpath({
-      d: `M${from.x} ${from.y} C${from.x + from.dx} ${from.y + from.dy}
-      ${to.x + to.dx} ${to.y + to.dy} ${to.x} ${to.y}`,
-      stroke: dragging ? 'black' : 'grey', fill: 'none', 'stroke-width': dragging ? 2 : 1,
-    });
+    return hg({},
+      hpath({
+        id: _id, 'marker-mid': 'url(#markerCross)',
+        d: `M${from.x} ${from.y} C${from.x + from.dx} ${from.y + from.dy}
+        ${to.x + to.dx} ${to.y + to.dy} ${to.x} ${to.y}`,
+        stroke: dragging ? 'black' : 'grey', fill: 'none', 'stroke-width': dragging || over ? 2 : 1,
+        onmouseover: e => {
+          console.log('path.over');
+          setIt({_id, over: true})
+        },
+        onmouseout: e => setIt({_id, over: false}),
+        onclick: e => {
+          if (e.shiftKey) deleteOne({_id});
+        },
+      }),
+      htext({'text-anchor': 'middle', dy: -2},
+        htextPath({startOffset: '50%', href: '#' + this._id}, this._id)
+      ),
+    );
   }
 }
 
@@ -273,6 +321,13 @@ const view = ({count, elems}, actions) => {
   }
   return hdiv({},
     hsvg({width: '800px', height: '800px', style: 'border: 1px solid red'},
+      hdefs({},
+        hmarker({id: 'markerCross', viewBox: '0 0 20 20', refX: 10, refY: 10, markerWidth: 20, markerHeight: 10},
+          hcircle({cx: 10, cy: 10, r: 10, fill: 'red', stroke: 'none'}),
+          hpath({d: 'M3 3 L17 17 M3 17 L17 3', fill: 'none', stroke: 'yellow'}),
+        )
+      ),
+      h('rect', {x: 0, y: 0, width: 800, height: 800, fill: '#EEE', stroke: 'black', onclick: e => actions.elems.setTopState({currentElem: null})}),
       hg({transform: 'translate(0.5, 0.5)'},
         [0, 1, 2, 3].map(layer => 
           hg({},
