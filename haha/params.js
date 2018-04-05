e1b5f909699f30c4cc50216efe89c03a4d96ec7e
Deneb42 <deneb42@gmail.com>
2018-04-05 17:44:17 +0300

    0.1

--- haha/params.js
+++ haha/params.js
@@ -18,7 +18,7 @@ export class ParamEditor {
 export class Float extends ParamEditor {
   renderEditor(h, actions, value) {
     const {name} = this;
-    return h('input', {name, type: 'number', value, onchange: e => {
+    return h('input', {name, type: 'number', step: 0.1, value, onchange: e => {
       actions.setElemProps({[this.name]: e.target.value});
       //console.log(e.target.value);
     }});
